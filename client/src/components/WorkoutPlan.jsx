4e997de83b68bdb9706cc2b43dc6071f34ddb7fd
IrishAir <bls21@mail.ru>
2022-03-10 07:35:09 +0500

    workout plan links to exercise

--- client/src/components/WorkoutPlan.jsx
+++ client/src/components/WorkoutPlan.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import workoutPlan from '../workoutPlan.json';
+import { Link } from 'react-router-dom';
 
 export default function WorkoutPlan() {
 
   return (
     <div className="profile-workout-plan">
       <h2>Программа тренировок</h2>
-      <div className="profile-workout-plan__wrapper flex jcsb">
+      <div className="flex jcsb">
 
         {workoutPlan.map(item => 
           <div className="card">
             <h3 className='regular'>{item.day}</h3>
             <ul className="links">
               {item.exercises.map(exercise => 
-                <li className='flex'><span>{exercise.order}.</span><p>{exercise.name}</p></li>
+                <Link className="flex link" key={exercise.id} to={`/exercises/${exercise.id}`} state= {exercise.id}>
+                  <span>{exercise.order}.</span>{exercise.name}
+                </Link>      
                 )}
             </ul>
-          </div>        
+          </div>  
           )}
-
       </div>      
     </div>
   )
