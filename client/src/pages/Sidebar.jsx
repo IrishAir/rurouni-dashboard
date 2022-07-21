2674866461956b2276b7e254704c21f9a53fc3f2
IrishAir <bls21@mail.ru>
2022-07-21 08:58:47 +0500

    responsive design: sidebar

--- client/src/pages/Sidebar.jsx
+++ client/src/pages/Sidebar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export default function Sidebar({isAuthenticated}) {
    return (
-    <div className="sidebar">
+    <div className="sidebar  flex cols">
       <div className="menu flex cols">
         <NavLink className="menu__profile" to='/'><div></div></NavLink>
         <NavLink className="menu__exercises" to='/exercises'><div></div></NavLink>
